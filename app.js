3907acde3fa5d0552a58dbe806b84d01542fdd59
dienigma <joshichinmay09@gmail.com>
2019-02-22 16:14:30 +0530

    Added Validators

--- app.js
+++ app.js
@@ -5,11 +5,10 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 let passport = require('passport');
 let session = require('express-session');
-
-require('./passport_setup')(passport);
-
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+
+require('./passport_setup')(passport);
 
 var app = express();
 
@@ -23,9 +22,10 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(session({secret: "adsasdasdlaksd"}))
+app.use(session({ secret: 'our new secret'}));
 app.use(passport.initialize());
 app.use(passport.session());
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
--- controllers/user.js
+++ controllers/user.js
@@ -1,43 +1,69 @@
-let models = require('../models')
-let bcrypt = require('bcrypt')
+let models = require("../models");
+let bcrypt = require("bcrypt");
 const passport = require('passport');
 const myPassport = require('../passport_setup')(passport);
-const flash = require('connect-flash');
+let flash = require('connect-flash');
+const {isEmpty} = require('lodash');
+const { validateUser } = require('../validators/signup');
 
-exports.show_login = (req,res, next) => {
-    res.render('user/login',{formData:{}, errors:{}});
+exports.show_login = function(req, res, next) {
+	res.render('user/login', { formData: {}, errors: {} });
 }
 
-exports.show_signup = (req,res, next) => {
-    res.render('user/signup',{formData:{}, errors:{}});
+exports.show_signup = function(req, res, next) {
+	res.render('user/signup', { formData: {}, errors: {} });
 }
 
-const generateHash = (password) => {
-    return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
+const rerender_signup = function(errors, req, res, next) {
+	res.render('user/signup', { formData: req.body, errors: errors});
+}
+const generateHash = function(password) {
+	return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null)
 }
 
-exports.signup = (req,res,next) => {
-    const newUser = models.User.build({
-        email:req.body.email,
-        password: generateHash(req.body.password)
-    });
-    return newUser.save().then(result => {
-        passport.authenticate('local',{
-            successRedirect: "/",
-            failureRedirect:"/signup",
-            failureFlash: true
-        })(req,res,next)
-    })
+exports.signup = function(req, res, next) {
+	let errors = {};
+	return validateUser(errors, req).then(errors => {
+		if (!isEmpty(errors)) {
+			rerender_signup(errors, req, res, next);
+		} else {
+			return models.User.findOne({
+				where: {
+					is_admin: true
+				}
+			}).then(user => {
+				let newUser;
+				if (user !== null) {
+					newUser = models.User.build({
+						email: req.body.email,
+						password: generateHash(req.body.password)
+					});					
+				} else {
+					newUser = models.User.build({
+						email: req.body.email,
+						password: generateHash(req.body.password),
+						is_admin: true
+					});
+				}
+				return newUser.save().then(result => {
+					passport.authenticate('local', {
+						successRedirect: "/",
+						failureRedirect: "/signup",
+						failureFlash: true
+					})(req, res, next);
+				})	
+			})		
+		}
+	})
 }
 
-exports.login = (req,res,next) => {
-    passport.authenticate('local',{
-        successRedirect: "/",
-        failureRedirect:"/login",
-        failureFlash: true
-    })(req,res,next)
+exports.login = function(req, res, next) { 
+	passport.authenticate('local', {
+		successRedirect: "/",
+		failureRedirect: "/login",
+		failureFlash: true
+	})(req, res, next);
 }
-
 exports.logout = function(req, res, next) { 
 	req.logout();
 	req.session.destroy();
