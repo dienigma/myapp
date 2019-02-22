daa72816ba69f7bd9210e76234a2ce9406c888fe
dienigma <joshichinmay09@gmail.com>
2019-02-22 08:12:54 +0530

    Initial Commit

---
+++ routes/index.js
@@ -0,0 +1,7 @@
+var express = require('express');
+var router = express.Router();
+var index = require('../controllers/index');
+/* GET home page. */
+router.get('/', index.index);
+
+module.exports = router;
