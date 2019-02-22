d3a0bb09d99d60d3fd385206287457ca440007d7
dienigma <joshichinmay09@gmail.com>
2019-02-22 15:19:23 +0530

    Implement passport local strategy

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+let passport = require('passport');
+let session = require('express-session');
+
+require('./passport_setup')(passport);
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -19,6 +23,9 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(session({secret: "adsasdasdlaksd"}))
+app.use(passport.initialize());
+app.use(passport.session());
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
---
+++ passport_setup.js
@@ -0,0 +1,51 @@
+let LocalStrategy = require('passport-local').Strategy;
+let bcrypt = require('bcrypt');
+let models = require('./models')
+
+const validPassword = function(user,password){
+    return bcrypt.compareSync(password,user.password);
+}
+
+module.exports = function(passport){
+    passport.serializeUser(function(user, done){
+        done(null,user.id)
+    });
+    passport.deserializeUser(function(id,done){
+        models.User.findOne({
+            where:{
+                'id': id
+            }
+        }).then(user =>{
+            if(user==null) {
+                done(new Error ("Wrong User ID."));
+            }
+            done(null,user);
+        })
+    });
+    passport.use(new LocalStrategy({
+        usernameField : 'email',
+        passwordField: 'password',
+        passReqToCallback: true
+    },
+    function(req,email,password,done){
+        return models.User.findOne({
+            where:{
+                'email':email
+            }
+        }).then(user => {
+            if (user == null){
+                req.flash('message','Incorrect Credentials');
+                return done(null,false);
+            } else if (user.password == null || user.password == undefined){
+                req.flash('message','Reset your password');
+                return done(null,false);
+            } else if (!validPassword(user,password)){
+                req.flash('message','Incorrect Credentials');
+                return done(null,false);
+            }
+            return done(null,user)
+        }).catch(err => {
+            done(err,false);
+        })
+    }))
+}
