169af249f96b0c116a08b717982d7c37b486dd9b
dienigma <joshichinmay09@gmail.com>
2019-02-22 08:24:20 +0530

    Added forms

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
-var index = require('../controllers/index');
+var landing = require('../controllers/landing');
 /* GET home page. */
-router.get('/', index.index);
+router.get('/', landing.get_landing);
 
 module.exports = router;
