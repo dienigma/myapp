dc75a661f141edf09097c147f753a6adea99d6b8
dienigma <joshichinmay09@gmail.com>
2019-02-22 08:33:33 +0530

    Landing page post route to submit email

--- controllers/landing.js
+++ controllers/landing.js
@@ -1,3 +1,7 @@
 exports.get_landing = function(req, res, next) {
   res.render('landing', { title: 'Express' });
 }
+exports.submit_lead = function(req, res, next) {
+  console.log("Lead email:" ,req.body.lead_email);
+  res.redirect('/');
+}
--- routes/index.js
+++ routes/index.js
@@ -3,5 +3,5 @@ var router = express.Router();
 var landing = require('../controllers/landing');
 /* GET home page. */
 router.get('/', landing.get_landing);
-
+router.post('/', landing.submit_lead)
 module.exports = router;
